a5c46c8a1f86bb1ad7fcc00aaeef203330572bb8
ougarcia <oz.ulysses@gmail.com>
2015-08-22 14:12:04 -0700

    first pass at move game class to es6

--- es6/game.js
+++ es6/game.js
@@ -1,9 +1,9 @@
-(function () {
-  if (typeof window.Asteroids === "undefined") {
-    window.Asteroids = {};
-  }
+import Bullet from './bullet';
+import Asteroid from './asteroid';
+import Ship from './ship';
 
-  var Game = window.Asteroids.Game = function (xDim, yDim) {
+class Game {
+  constructor(xDim, yDim) {
     this.ctx = window.Asteroids.ctx;
     this.NUM_ASTEROIDS = 4;
     this.lives = 3;
@@ -15,55 +15,48 @@
       this.addAsteroids();
     }
     this.addShip();
-  };
-
-  Game.prototype.randomPosition = function () {
+  }
+  randomPostion() {
     var x = Math.floor(Math.random() * this.DIM_X);
     var y = Math.floor(Math.random() * this.DIM_Y);
     return [x, y];
-  };
-
-  Game.prototype.addAsteroids = function() {
+  }
+  addAsteroids() {
     var position1 = this.randomPosition();
     var position2 = this.randomPosition();
     var position3 = this.randomPosition();
     this.asteroids.push(Asteroids.Asteroid.newBigAsteroid({"pos": position1}));
-  };
-
-  Game.prototype.addShip = function () {
+  }
+  addShip() {
     var position = this.randomPosition();
     this.ship = new Asteroids.Ship({ "pos": position });
-  };
-
-  Game.prototype.allObjects = function () {
+  }
+  allObjects() {
     var allObjectsArray = this.asteroids.slice(0);
     this.ship && allObjectsArray.unshift(this.ship);
     allObjectsArray = allObjectsArray.concat(this.bullets);
     return allObjectsArray;
-  };
+  }
 
-  Game.prototype.draw = function() {
+  draw() {
     this.ctx.clearRect(0, 0, this.DIM_X, this.DIM_Y);
     this.drawLives();
     this.allObjects().forEach(function(object) {
       object.draw(this.ctx);
     }.bind(this));
-  };
-
-  Game.prototype.drawLives = function () {
+  }
+  drawLives() {
     this.ctx.fillStyle = "Black";
     this.ctx.font = "48px serif";
     this.ctx.fillText(this.lives + " lives", 0, 50);
-  };
-
-  Game.prototype.moveObjects = function () {
+  }
+  moveObjects() {
     this.allObjects().forEach(function(object) {
       object.move();
     });
-  };
-
-  Game.wrap = function(pos, object) {
-    //TODO remove object from dom and instance variables if it's outside the DOM
+  }
+  wrap(pos, object) {
+  //TODO remove object from dom and instance variables if it's outside the DOM
     if (object.isWrappable) {
       for(var i = 0; i < 2; i++) {
         if (pos[i] > window.Asteroids.dims[i]) {
@@ -74,9 +67,8 @@
       }
     }
     return pos;
-  };
-
-  Game.prototype.checkCollisions = function () {
+  }
+  checkCollsisions() {
     var that = this;
     this.bullets.forEach(function(bullet) {
       that.asteroids.forEach(function(asteroid) {
@@ -91,28 +83,23 @@
         that.resetShip();
       }
     });
-  };
-
-  Game.prototype.step = function () {
+  }
+  step() {
     this.moveObjects();
     this.checkCollisions();
-  };
-
-  Game.prototype.removeAsteroid = function (asteroid) {
+  }
+  removeAsteroid(asteroid) {
       var asteroidIndex = this.asteroids.indexOf(asteroid);
       this.asteroids.splice(asteroidIndex, 1);
       this.asteroids = this.asteroids.concat(asteroid.spawnChildren());
-  };
-
-  Game.prototype.removeBullet = function(bullet) {
+  }
+  removeBullet(bullet) {
     var bulletIndex = this.bullets.indexOf(bullet);
     this.bullets.splice(bulletIndex, 1);
-  };
-
-  Game.prototype.resetShip = function() {
+  }
+  resetShip() {
     delete this.ship;
     this.lives--;
     this.lives && setTimeout(this.addShip.bind(this), 1000);
-  };
-
-})();
+  }
+}
